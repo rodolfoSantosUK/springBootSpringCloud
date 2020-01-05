d81d5cb175b471e3dab17b0f5a4554a7991452bf
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-01-05 20:59:20 -0300

    Usando PathVariable

--- restful-web-services/src/main/java/com/rest/wewbsesrvices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/wewbsesrvices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.wewbsesrvices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.wewbsesrvices.restfulwebservices.domain.HelloWorldBean;
@@ -16,6 +17,11 @@ public class HelloWorldController {
 	@GetMapping( path= "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Usando Hello World beans");
+	}
+	
+	@GetMapping( path= "/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean( String.format("Hello  World, %s",  name));
 	}
 	
 }
