36492ef06f579c6c1a4553b38b16bcdc92633232
Rodolfo Santos <santoseuro2018@gmail.com>
2020-01-11 05:46:09 -0500

    Trabalhando com internacionalização

--- restful-web-services/src/main/java/com/rest/wewbsesrvices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/wewbsesrvices/restfulwebservices/HelloWorldController.java
@@ -1,28 +1,41 @@
 package com.rest.wewbsesrvices.restfulwebservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.wewbsesrvices.restfulwebservices.domain.HelloWorldBean;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
-	@GetMapping( path= "/hello-world")
-	public String helloWorld() {
-		return "hello world" ;
-	}
-	
-	@GetMapping( path= "/hello-world-bean")
-	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Usando Hello World beans");
-	}
-	
-	@GetMapping( path= "/hello-world-bean/path-variable/{name}")
-	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
-		return new HelloWorldBean( String.format("Hello  World, %s",  name));
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "hello world";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Usando Hello World beans");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello  World, %s", name));
+    }
+
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false)  Locale locale) {
+		return messageSource.getMessage("good.morning.message", null ,locale) ;
 	}
 
-	
 }
--- restful-web-services/src/main/java/com/rest/wewbsesrvices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/rest/wewbsesrvices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,12 +2,37 @@ package com.rest.wewbsesrvices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+
+    @Bean
+    LocaleResolver localeResolver() {
+
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.UK);
+
+        return sessionLocaleResolver;
+    }
+
+   @Bean
+   public ResourceBundleMessageSource messageSource() {
+       ResourceBundleMessageSource  messageSource =  new ResourceBundleMessageSource();
+       messageSource.setBasename("messages");
+       return messageSource;
+   }
+
+
 
 }
